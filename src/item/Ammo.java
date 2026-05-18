d2a9adc50ea6052b277b66ff335a594ad1af330e
aulmtqn <lubisaulia888@gmail.com>
2026-05-18 09:39:23 +0700

    fix lagii

--- src/item/Ammo.java
+++ src/item/Ammo.java
@@ -1,19 +1,19 @@
-//package item;
-//
-//import entity.Player;
-//
-//public class Ammo extends Items {
-//    private int ammoAmount = 30;
-//
-//    public Ammo() {
-//        setName("Ammo Box");
-//    }
+package item;
 
-//    @Override
-//    public void use(Player player) {
-//        if (player.getWeapon() != null) {
-//            player.getWeapon().addAmmo(this.ammoAmount);
-//            System.out.println("Berhasil reload! Amunisi bertambah: " + ammoAmount);
-//        }
-//    }
-//}
+import entity.Player;
+
+public class Ammo extends Items {
+    private int ammoAmount = 30;
+
+    public Ammo() {
+        setName("Ammo Box");
+    }
+
+    @Override
+    public void use(Player player) {
+        if (player.getWeapon() != null) {
+            player.getWeapon().addAmmo(this.ammoAmount);
+            System.out.println("Berhasil reload! Amunisi bertambah: " + ammoAmount);
+        }
+    }
+}
